0730db42b20d385f8c71dad28b89a762d8757440
BakaCommitDa <3076323533@qq.com>
2025-05-29 23:03:51 +0800

    红包算法

---
+++ algorithm/big_num_add/1.js
@@ -0,0 +1,23 @@
+/**
+ * 1. 两数之和
+ * @param {String} num1
+ * @param {String} num2
+ * @return {String} 
+ */
+
+function adddLargeNumbers(num1,num2){
+    let result = '';//存储结果
+    let carry = 0;//存储进位 
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+    while(i >= 0||j >=0||carry >0){
+        //考虑边界
+        const digit1 = i >= 0 ?parseInt(num1[i]):0;
+        const digit2 = j >= 0 ?parseInt(num2[i]):0;
+        const sum = digit1 + digit2 + carry;
+        result = sum % 10 + result;
+        carry = Math.floor(sum/10);
+        i--;
+        j--;
+    }
+}
---
+++ algorithm/big_num_add/readme.md
@@ -0,0 +1,16 @@
+# 大数相加
+
+- 高精度 
+  js 只有Number 类型，不分整数、浮点数、高精度...
+  js 不太适合计算  python  适合计算
+  js的表现力强
+- 大数字
+  存在一个边界问题
+  Infinity
+  -Infinity
+  Number.MAX_VALUE
+
+- 先字符串化
+  
+
+- es6  提供了bigInt  大数类型
