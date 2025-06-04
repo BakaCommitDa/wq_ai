6e99c4dee0f47a714b958fbfe06db0492321516e
BakaCommitDa <3076323533@qq.com>
2025-06-04 23:23:29 +0800

    react

---
+++ react/readme.md
@@ -0,0 +1,60 @@
+# react 业务
+friends 数据
+App
+挂载点
+页面的显示
+- 原生JS
+  DOM 编程
+  底层API 不够高效 
+  聚焦于业务
+  前端切图载 （html,css + 一点js）-》 前端开发工程师（微博，美团）JS框架（vue/react）
+  -》 node 全栈开发 （node + 数据库）-》react native Android + ios App->AIGC AI应用
+  -》 全干工程师
+  Web应用（www.baidu.com）  移动应用（Android,IOS）
+  Node (server)  AI 统领一切 JS AI全干工程师
+
+
+## **项目**的创建
+- npm 是什么？ node package manager 
+  node 包管理工具 安装react package 提供App开发能力
+- npm init vite 
+  初始化项目 init
+  vite 提供vue/react 项目模板和**工程**化
+- 选择一些配置
+  - react
+  - js 
+  得到项目模板， 基于它开始开发
+  - cd 项目名/
+  - npm i  安装依赖
+    node_modules  包所在
+  - npm run dev 启动项目
+    3000 端口启动应用
+
+    
+
+# 5 w
+- what Web App
+- how npm init vite 初始化并vite的项目模板
+- 安装依赖
+- 启动应用 http 5173    react 技术栈 Web App   
+远离JS API 编程， 面向于业务 
+
+## 响应式业务 
+## TODOS 任务列表
+   - 数据 ['脱单'，'学习'，'健身']
+   数据在页面上渲染  react 提供点啥 支持这个业务 
+
+## react 初体验
+- react 组件是完成开发任务的最小单元
+- 组件是一个函数
+- 返回一个html
+- 函数体里面 return 之前可以声明数据和业务逻辑
+- {}  js  表达式   不需要DOM  API 编程
+
+
+## 响应式数据
+- 数据会发生改变的，数据状态 state
+- [todos,setTodos] = useState(初始值) 使用一个数据状态
+   数组第一项 数据项
+   数据第二项目 修改数据项的方法
+
---
+++ react/todos/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import './App.css'
+//todos 列表需要渲染
+// 函数组件  APP组件 组合其他的组件完成应用
+//返回html 的函数
+//html css js 用函数组合在一起，  组件
+// function App() {
+//   //react 比 vue 更纯粹
+//   const todos = ['吃饭','睡觉','打豆豆'];// 数组-》 数据
+//   return (
+//     <>
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>序号</th>
+//           <th>任务</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {
+//           // 动态的
+//           // react 一个括号
+//           // js DOM编程
+//           // 在html 里面写js 代码
+//           todos.map((item,index)=>
+//           <tr>
+//             <td>{index+1}</td>
+//             <td>{item}</td>
+//             </tr>
+          
+//         )
+//         }
+//       </tbody>
+//     </table>
+
+//     </>
+//   )
+// }
+
+function App(){
+  // 数据 -》 数据状态  数据业务 数据是改变的  称为数据状态
+  const [todos,setTodos] = useState(['吃饭','睡觉','打豆豆']);
+  const [title,setTitle] = useState('ECUT 小明');
+  setTimeout(()=>{
+    setTodos(['吃饭','睡觉','打豆豆','养鱼']);
+    setTitle('字节 小明')
+  },3000);
+  setTimeout(()=>{
+    setTitle('北邮 小明')
+  },5000)
+  return(
+    <div>
+      <h1 className='title'>{title}</h1>
+    
+    <table>
+      <thead>
+        <tr>
+          <th>序号</th>
+          <th>任务</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          // html 模板
+          todos.map((item,index)=>
+          <tr>
+            <td>{index+1}</td>
+            <td>{item}</td>
+            </tr>
+        )
+        }
+      </tbody>
+    </table
+    ></div>
+  )
+}
+export default App
