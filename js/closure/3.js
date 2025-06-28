d06590b82ea5cee97193356714e48bc6d43cbfef
BakaCommitDa <3076323533@qq.com>
2025-06-28 22:21:05 +0800

    闭包深度

--- js/closure/3.js
+++ js/closure/3.js
@@ -3,11 +3,6 @@ function add(...args){
     return (...newArgs)=>{
         const arr =  add(...args,...newArgs);
         console.log(arr);
-        return (...newargs)=>{
-            const arr =  add(...args,...newArgs,...newargs);
-            console.log(arr);
-            
-        }
     }
 }
-add(1,2,3)(4,5,6)(7,8,9)
+add(1,2,3)(4,5,6)
